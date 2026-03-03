5a7b4ea3f537e8a5e00b30b99db9e207a1c7d076
John Steendijk <johnsteendijk@macbook-air-van-john-2.home>
2026-03-03 11:07:31 +0100

    Initial commit: blog GitHub Pages site

---
+++ posts.js
@@ -0,0 +1,12 @@
+const posts = [
+  {
+    date: "2026-03-03",
+    title: "First post",
+    body: "Welcome to my blog. Here I share short thoughts and texts."
+  },
+  {
+    date: "2026-03-03",
+    title: "How it works",
+    body: "Adding new texts is simple: open posts.js and add a new object at the top of the list. That's all."
+  }
+];
